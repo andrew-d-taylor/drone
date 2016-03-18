7e1103771dece9c9e1ac8f6a2152d8b640baf9fb
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2016-03-18 15:44:31 -0400

    Default to a date-descending table ordering

--- controllers.js
+++ controllers.js
@@ -6,7 +6,7 @@
     controllers.controller('MainCtrl', ['$scope', 'DroneAPIService', 'uiGmapGoogleMapApi', function($scope, DroneAPIService, uiGmapGoogleMapApi) {
 
         $scope.orderByField = 'date';
-        $scope.reverseSort = false;
+        $scope.reverseSort = true;
 
         $scope.setOrder = function(value) {
             $scope.orderByValue = value;
